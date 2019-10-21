0857e9a2fb4bec4b37bc827756fc75e3bbbd53da
Jared Hughes <jahughes241@gmail.com>
2019-10-20 20:34:06 -0700

    Move servo on scale 0 (released) to 1 (pressed)

--- grbl/config.h
+++ grbl/config.h
@@ -257,8 +257,14 @@
 // equally divided voltage bins between the maximum and minimum spindle speeds. So for a 5V pin, 1000
 // max rpm, and 250 min rpm, the spindle output voltage would be set for the following "S" commands: 
 // "S1000" @ 5V, "S250" @ 0.02V, and "S625" @ 2.5V (mid-range). The pin outputs 0V when disabled.
-#define SPINDLE_MAX_RPM 1000.0 // Max spindle RPM. This value is equal to 100% duty cycle on the PWM.
-#define SPINDLE_MIN_RPM 0.0    // Min spindle RPM. This value is equal to (1/256) duty cycle on the PWM.
+#ifndef RC_SERVO
+  #define SPINDLE_MAX_RPM 1000.0 // Max spindle RPM. This value is equal to 100% duty cycle on the PWM.
+  #define SPINDLE_MIN_RPM 0.0    // Min spindle RPM. This value is equal to (1/256) duty cycle on the PWM.
+#else
+  #define SPINDLE_MAX_RATIO 0.25 // ratio of total distance that the servo should be when all the way down
+  #define SPINDLE_MAX_RPM 1.0/SPINDLE_MAX_RATIO // Max spindle RPM. This value is equal to 100% duty cycle on the PWM.
+  #define SPINDLE_MIN_RPM 0.0    // Min spindle RPM. This value is equal to (1/256) duty cycle on the PWM.
+#endif
 
 // Used by variable spindle output only. This forces the PWM output to a minimum duty cycle when enabled.
 // When disabled, the PWM pin will still read 0V. Most users will not need this option, but it may be 
