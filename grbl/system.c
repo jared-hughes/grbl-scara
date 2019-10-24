64414c2c0c8abc4c6450e232ddeb6ae04d3a13f5
Jared Hughes <jahughes241@gmail.com>
2019-10-23 17:19:07 -0700

    [SCARA] Use quick_atan2 instead of atan2
    Should be able to save more time wtih less trig

--- grbl/system.c
+++ grbl/system.c
@@ -299,6 +299,43 @@ void system_convert_array_steps_to_mpos(float *position, int32_t *steps)
 }
 
 #ifdef SCARA
+// https://www.dsprelated.com/showarticle/1052.php
+float quick_atan2(float y, float x)
+{
+  const float n1 = 0.97239411f;
+  const float n2 = -0.19194795f;    
+  float result = 0.0f;
+  if (x != 0.0f) {
+    const union { float flVal; uint32_t nVal; } tYSign = { y };
+    const union { float flVal; uint32_t nVal; } tXSign = { x };
+    if (fabsf(x) >= fabsf(y)) {
+      union { float flVal; uint32_t nVal; } tOffset = { M_PI };
+      // Add or subtract PI based on y's sign.
+      tOffset.nVal |= tYSign.nVal & 0x80000000u;
+      // No offset if x is positive, so multiply by 0 or based on x's sign.
+      tOffset.nVal *= tXSign.nVal >> 31;
+      result = tOffset.flVal;
+      const float z = y / x;
+      result += (n1 + n2 * z * z) * z;
+    }
+    else { // Use atan(y/x) = pi/2 - atan(x/y) if |y/x| > 1.
+      union { float flVal; uint32_t nVal; } tOffset = { M_PI/2 };
+      // Add or subtract PI/2 based on y's sign.
+      tOffset.nVal |= tYSign.nVal & 0x80000000u;            
+      result = tOffset.flVal;
+      const float z = x / y;
+      result -= (n1 + n2 * z * z) * z;            
+    }
+  }
+  else if (y > 0.0f) {
+    result = M_PI/2;
+  }
+  else if (y < 0.0f) {
+    result = -M_PI/2;
+  }
+  return result;
+}
+
 void system_convert_mpos_to_array_steps(int32_t *target_steps, float* position)
 {
   uint8_t idx;
@@ -316,11 +353,11 @@ void system_convert_mpos_to_array_steps(int32_t *target_steps, float* position)
   //   || r > settings.upper_arm + settings.lower_arm) {
   //   return target_scara;
   // }
-  float theta = 2 * atan2(sqrt(r_squared - sm), sqrt(s - r_squared));
-  target_steps[U_MOTOR] = lround(settings.steps_per_mm[U_MOTOR] * (atan2(y, x) +
-    atan2(2 * l1 * l2 * sin(theta), l1_squared + r_squared - l2_squared)));
-  target_steps[L_MOTOR] = lround(settings.steps_per_mm[L_MOTOR] * (theta - M_PI))
-    + target_steps[U_MOTOR];
+  float theta = 2 * quick_atan2(sqrt(r_squared - sm), sqrt(s - r_squared));
+  float angle = quick_atan2(y, x) + quick_atan2(2 * l1 * l2 * sin(theta),
+    l1_squared + r_squared - l2_squared);
+  target_steps[U_MOTOR] = lround(settings.steps_per_mm[U_MOTOR] * angle);
+  target_steps[L_MOTOR] = lround(settings.steps_per_mm[L_MOTOR] * (theta - M_PI + angle));
   for (idx=0; idx<N_AXIS; idx++) {
     if (idx != U_MOTOR && idx != L_MOTOR) {
       target_steps[idx] = lround(position[idx] * settings.steps_per_mm[idx]);
